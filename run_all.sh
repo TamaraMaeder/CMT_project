03f188bc80c40268b29047539aa0c066d2415168
Emma Willaume <emma.willaume@epfl.ch>
2025-12-10 18:22:44 +0100

    update shell

--- run_all.sh
+++ run_all.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-echo "=== Step 1: Generating aerosol profiles (Python) ==="
-python3 scripts/step1_generate_profiles.py
+echo "=== Step 0: Generate aerosol parameters CSV (C program) ==="
+gcc src/c/aerosols_param.c -o aerosols_param
+./aerosols_param
+echo "CSV generated."
+
+echo "=== Step 1: Generating lognormal distribution and plot then generate aerosol profiles and plot (Python) ==="
+python3 src/python/height_vs_radius.py
 if [ $? -ne 0 ]; then
     echo "Error during Step 1."
     exit 1
@@ -18,7 +23,7 @@ fi
 
 echo ""
 echo "=== Step 3: Final plots and analysis (Python) ==="
-python3 scripts/step3_final_plots.py
+python3 src/python/graph.py
 if [ $? -ne 0 ]; then
     echo "Error during Step 3."
     exit 1
