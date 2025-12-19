8b3066aa5c212c2fb89cf4aed3dc4f6b0d3d51f1
Emma Willaume <emma.willaume@epfl.ch>
2025-12-19 10:31:26 +0100

    shell_out

--- src/run_all.sh
+++ src/run_all.sh
@@ -1,38 +1,3 @@
-# #!/bin/bash
-
-# echo "=== Step 0: Generate aerosol parameters CSV (C program) ==="
-# gcc src/c/aerosols_param.c -o aerosols_param
-# ./aerosols_param
-# echo "CSV generated."
-
-# echo "=== Step 1: Generating lognormal distribution and plot then generate aerosol profiles and plot (Python) ==="
-# python3 src/python/height_vs_radius.py
-# if [ $? -ne 0 ]; then
-#     echo "Error during Step 1."
-#     exit 1
-# fi
-
-# echo ""
-# echo "=== Step 2: Computing cloud albedo (C program) ==="
-# gcc src/c/process_all.c -o process_all
-# ./albedo_fct
-# if [ $? -ne 0 ]; then
-#     echo "Error during Step 2."
-#     exit 1
-# fi
-
-# echo ""
-# echo "=== Step 3: Final plots and analysis (Python) ==="
-# python3 src/python/plot_albedo_by_scenario.py
-# if [ $? -ne 0 ]; then
-#     echo "Error during Step 3."
-#     exit 1
-# fi
-
-# echo ""
-# echo "All simulations successfully completed!"
-
-
 #!/bin/bash
 set -e  # stoppe le script à la première erreur
 
@@ -79,10 +44,11 @@ echo "[build] albedo_fn.c + albedo_run.c -> a.out"
 gcc -std=c11 -O2 -Wall -Wextra \
     "$SRC_C/albedo_fn.c" \
     "$SRC_C/albedo_run.c" \
+    -o bin/albedo \
     -lm
 
 echo "[run] a.out (albedo_run)"
-./a.out
+./bin/albedo
 echo
 
 # Build & run albedo_vs_N (paired with albedo_fn.c)
@@ -90,10 +56,11 @@ echo "[build] albedo_fn.c + albedo_vs_N_run.c -> a.out"
 gcc -std=c11 -O2 -Wall -Wextra \
     "$SRC_C/albedo_fn.c" \
     "$SRC_C/albedo_vs_N_run.c" \
+    -o bin/albedo_N \
     -lm
 
 echo "[run] a.out (albedo_vs_N_run)"
-./a.out
+./bin/albedo_N
 echo
 
 # Build & run effradius (effective_radius.c + apply_effective_radius.c)
@@ -101,10 +68,11 @@ echo "[build] effective_radius.c + apply_effective_radius.c -> a.out"
 gcc -std=c11 -O2 -Wall -Wextra \
     "$SRC_C/effective_radius.c" \
     "$SRC_C/apply_effective_radius.c" \
+    -o bin/albedo_r \
     -lm
 
 echo "[run] a.out (effradius)"
-./a.out
+./bin/albedo_r
 
 echo "=== Step 3: Final plots and analysis (Python) ==="
 
